bd5c2d2745f82c38351610ede58faa885d708061
paddy@pj-desktop <patrick.james.robinson@proton.me>
2026-06-08 23:49:40 +0100

    update hash

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
 
         # First build will intentionally fail and tell you the real hash.
         # Replace this with the hash from the error message.
-        vendorHash = "sha256-uwBJAqN4sIepiiJf9lCDumLqfKJEowQO2tOiSWD3Fig=";
+        vendorHash = "sha256-CowlwrEFU8x1f2d99uccRKSwjA3NhqNWL4yin1j0vNs=";
 
         # If your main package is under ./cmd/foxden
         subPackages = ["cmd/foxden"];
